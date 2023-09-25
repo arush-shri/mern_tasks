fa3ab46ec9aef93c856563a2ad4d21877e714491
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-09-25 19:16:08 +0530

    js task removed

--- task/jsTask1.js
+++
@@ -1,8 +0,0 @@
-function reverseNum(num) {
-    const reversedStr= num.toString().split('').reverse().join('');
-    const reversedNum = parseFloat(reversedStr);
-
-    return reversedNum;
-}
-
-console.log(reverseNum(32243))
--- task/jsTask2.js
+++
@@ -1,1 +0,0 @@
-console.log("webmaster".split("").sort().join(""))
